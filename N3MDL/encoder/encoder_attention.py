c5bcead5f52dcb9672a96ff6c9aa204e3f723528
next2523 <jordan.dean161@gmail.com>
2024-09-16 08:49:14 -0400

    Completed initial encoder attention

--- N3MDL/encoder/encoder_attention.py
+++ N3MDL/encoder/encoder_attention.py
@@ -1,12 +1,15 @@
-import torch
 import torch.nn as nn
-import torch.optim as optim
-import torch.utils.data as data
-import math
-import copy
-import numpy as np
-import matplotlib.pyplot as plt
+from attention import attention
 
 class EncoderAttention(nn.Module):
-    def __init__(self):
+    def __init__(self, model_dimensionality, num_heads, dropout):
         super(self, EncoderAttention).__init__()
+
+        self.attention = attention.AttentionMechanism(model_dimensionality, num_heads)
+        self.normalization = nn.LayerNorm(model_dimensionality)
+
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, mask):
+        attention_output = self.attention(x, x, x, mask)
+        x = self.normalization(x + self.dropout(attention_output))

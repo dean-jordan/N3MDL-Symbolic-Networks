0c7819ac9f39d8e806609e406a3deb79c3c2acc7
next2523 <jordan.dean161@gmail.com>
2024-09-18 09:04:53 -0400

    Developed feedforward networks for encoder/decoder

--- N3MDL/decoder/decoder_feedforward.py
+++ N3MDL/decoder/decoder_feedforward.py
@@ -5,3 +5,31 @@ from layers import decoder_recurrent
 class DecoderFeedForwardNetwork(nn.Module):
     def __init__(self):
         super(DecoderFeedForwardNetwork, self).__init__()
+
+        self.recurrent1 = decoder_recurrent.DecoderRecurrentLayer(input_size=2, hidden_size=3, output_size=2)
+        self.recurrent2 = decoder_recurrent.DecoderRecurrentLayer(input_size=2, hidden_size=3, output_size=2)
+        self.recurrent3 = decoder_recurrent.DecoderRecurrentLayer(input_size=2, hidden_size=3, output_size=2)
+        self.recurrent4 = decoder_recurrent.DecoderRecurrentLayer(input_size=2, hidden_size=3, output_size=2)
+        self.recurrent5 = decoder_recurrent.DecoderRecurrentLayer(input_size=2, hidden_size=3, output_size=2)
+        self.activation = relu.ReLU()
+
+    def forward(self, x):
+        self.recurrent5(
+            self.activation(
+                self.recurrent4(
+                    self.activation(
+                        self.recurrent3(
+                            self.activation(
+                                self.recurrent2(
+                                    self.activation(
+                                        self.recurrent1(
+                                            self.activation(x)
+                                        )
+                                    )
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+        )
--- N3MDL/encoder/encoder_feedforward.py
+++ N3MDL/encoder/encoder_feedforward.py
@@ -5,3 +5,31 @@ from layers import encoder_recurrent
 class EncoderFeedForwardNetwork(nn.Module):
     def __init__(self):
         super(EncoderFeedForwardNetwork, self).__init__()
+
+        self.recurrent1 = encoder_recurrent.EncoderRecurrentLayer(input_size=2, hidden_size=3, output_size=2)
+        self.recurrent2 = encoder_recurrent.EncoderRecurrentLayer(input_size=2, hidden_size=3, output_size=2)
+        self.recurrent3 = encoder_recurrent.EncoderRecurrentLayer(input_size=2, hidden_size=3, output_size=2)
+        self.recurrent4 = encoder_recurrent.EncoderRecurrentLayer(input_size=2, hidden_size=3, output_size=2)
+        self.recurrent5 = encoder_recurrent.EncoderRecurrentLayer(input_size=2, hidden_size=3, output_size=2)
+        self.activation = relu.ReLU()
+
+    def forward(self, x):
+        self.recurrent5(
+            self.activation(
+                self.recurrent4(
+                    self.activation(
+                        self.recurrent3(
+                            self.activation(
+                                self.recurrent2(
+                                    self.activation(
+                                        self.recurrent1(
+                                            self.activation(x)
+                                        )
+                                    )
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+        )
